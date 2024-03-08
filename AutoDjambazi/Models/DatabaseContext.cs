6f51a8b3b5a7c6fcf6de8932ef2c4d5f122da506
todor02 <todort02@abv.bg>
2024-03-08 02:02:54 +0200

    Created database Mobiphony on a localhost | Created List of <Phone>

---
+++ AutoDjambazi/Models/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AutoDjambazi.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
--- AutoDjambazi/Startup.cs
+++ AutoDjambazi/Startup.cs
@@ -1,6 +1,14 @@
+using AutoDjambazi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DatabaseContext>(options => 
+{
+    options.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=Mobiphony;Trusted_Connection=True;TrustServerCertificate=True;");
+});
 
 var app = builder.Build();
 
