8282f05e69e215cb9ff17ffb2adbe0e9c74d703e
Adam Ayash <mobiragegames@mail.com>
2024-04-05 21:54:55 +0300

    Adding card phones partial view

---
+++ Mobiphony/Controllers/Phones/PhonesController.cs
@@ -0,0 +1,38 @@
+﻿namespace Mobiphony.Controllers.Phones
+{
+	#region
+	using Microsoft.AspNetCore.Mvc;
+	#endregion
+
+	/// <summary></summary>
+	public class PhonesController : Controller
+	{
+		//-------------------------
+		//Constants:
+		//-------------------------
+
+		//-------------------------
+		//Members:
+		//-------------------------
+
+		//-------------------------
+		//Properties:
+		//-------------------------
+
+		//-------------------------
+		//Constructor/Destructor:
+		//-------------------------
+
+		//-------------------------
+		//Methods:
+		//-------------------------
+		public IActionResult Phones()
+		{
+			return View();
+		}
+		//-------------------------
+		//Overrides:
+		//-------------------------
+
+	}
+}
