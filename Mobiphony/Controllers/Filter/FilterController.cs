d797d3fdadcb510b85ee02ebbedae2d835bc2f6a
todor02 <todort02@abv.bg>
2024-04-05 23:39:52 +0300

    Making little changes

---
+++ Mobiphony/Controllers/Filter/FilterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mobiphony.Controllers.Filter
+{
+	public class FilterController : Controller
+	{
+		public IActionResult Filter()
+		{
+			return View();
+		}
+	}
+}
