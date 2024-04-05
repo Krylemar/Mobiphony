051eaef74099c7571c6987c61f9ca669d74e98f6
todor02 <todort02@abv.bg>
2024-04-05 21:21:02 +0300

    Adding and Rebasing Cards

---
+++ Mobiphony/Controllers/PhoneLogo/PhoneLogoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mobiphony.Controllers.Cards
+{
+	public class PhoneLogoController : Controller
+	{
+		public IActionResult CardLogo()
+		{
+			return View();
+		}
+	}
+}
