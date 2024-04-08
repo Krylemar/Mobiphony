2e9c1a95ee98b0c04c15e47f33a9736a2c2181e6
todor02 <todort02@abv.bg>
2024-04-08 22:00:09 +0300

    Created folders and files for Phones(all phones) visualisation

---
+++ Mobiphony/Controllers/Phones/PhonesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mobiphony.Controllers.Phones
+{
+	public class PhoneController : Controller
+	{
+		public IActionResult Phones()
+		{
+			return View();
+		}
+	}
+}
